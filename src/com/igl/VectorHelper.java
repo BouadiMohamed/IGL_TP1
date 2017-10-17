6563d0f0ee66467cd14c5bf745fac2ff9c59a89f
Bouadi Mohammed <fm_bouadi@esi.dz>
2017-10-17 21:40:37 +0100

    Added Max&Min

--- src/com/igl/VectorHelper.java
+++ src/com/igl/VectorHelper.java
@@ -7,52 +7,60 @@ import java.util.function.Function;
  */
 public class VectorHelper {
 
-        public static int[] trier(int[] tab){
-            int i=0;
-            int j=0;
-            int permut_count =0;
-            do {
-                permut_count = 0;
-                for(i=0;i<tab.length-1;i++){
-                    if(tab[i]>tab[i+1]){
-                        permut_count=1;
-                        j=tab[i];
-                        tab[i]=tab[i+1];
-                        tab[i+1]=j;
-                    }
+    public static int[] trier(int[] tab) {
+        int i = 0;
+        int j = 0;
+        int permut_count = 0;
+        do {
+            permut_count = 0;
+            for (i = 0; i < tab.length - 1; i++) {
+                if (tab[i] > tab[i + 1]) {
+                    permut_count = 1;
+                    j = tab[i];
+                    tab[i] = tab[i + 1];
+                    tab[i + 1] = j;
                 }
-            } while(permut_count !=0);
-            return tab;
-        }
-
-        public static int[] inverser (int[] tab){
-            int temp=0;
-
-            for (int i=0;i<tab.length/2;i++){
-                temp= tab[i];
-                tab[i]=tab[tab.length-i-1];
-                tab[tab.length-i-1]=temp;
             }
-            return tab;
+        } while (permut_count != 0);
+        return tab;
+    }
 
+    public static int[] inverser(int[] tab) {
+        int temp = 0;
+
+        for (int i = 0; i < tab.length / 2; i++) {
+            temp = tab[i];
+            tab[i] = tab[tab.length - i - 1];
+            tab[tab.length - i - 1] = temp;
         }
-    public static int[] sum(int[] v, int[] u) throws DifferentDimensionException
-     {
-               if(v.length != u.length) throw new DifferentDimensionException();
-               int[] w = new int[v.length];
-               for(int i = 0; i < w.length; i++)
-               {
-                    w[i] = v[i] + u[i];
-               }
-               return w;
-     }
+        return tab;
 
-    public static void apply(int v[], Function<Integer, Integer> m)
-    {
-               for(int i = 0; i < v.length; i++)
-               {
-                               v[i] = m.apply(v[i]);
-              }
-              }
+    }
+
+    public static int[] sum(int[] v, int[] u) throws DifferentDimensionException {
+        if (v.length != u.length) throw new DifferentDimensionException();
+        int[] w = new int[v.length];
+        for (int i = 0; i < w.length; i++) {
+            w[i] = v[i] + u[i];
+        }
+        return w;
+    }
+
+    public static void apply(int v[], Function<Integer, Integer> m) {
+        for (int i = 0; i < v.length; i++) {
+            v[i] = m.apply(v[i]);
+        }
+    }
+
+    public static int[] maxAndMin(int[] v) {
+        int res[] = new int[2];
+        res[0] = res[1] = v[0];
+        for (int i = 1; i < v.length; i++) {
+            if (v[i] > res[0]) res[0] = v[i];
+            if (v[i] < res[1]) res[1] = v[i];
+        }
+        return res;
+
+    }
 
 }
