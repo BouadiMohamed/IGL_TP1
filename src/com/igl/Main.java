fa2d56776680c125974fb3b0a1effeb49e3c73f4
Bouadi Mohammed <fm_bouadi@esi.dz>
2017-10-17 21:59:42 +0100

    Added MaxMin

--- src/com/igl/Main.java
+++ src/com/igl/Main.java
@@ -7,10 +7,10 @@ import static com.igl.VectorHelper.trier;
 public class Main {
 
     public static void main(String[] args) throws DifferentDimensionException {
-      int[] x={1,5,4,7};
+     int[] x={1,5,4,7};
       int[] z={4,2,4,10};
-      int [] y =new int[4] ;
-      y= sum(x,z);
+      int [] y = new int[4] ;
+      y= (x,z);
       System.out.print(y[0]+" "+ y[1]+ " "+ y[2]+" "+ y[3]);
 	// write your code here
     }
--- src/com/igl/VectorHelper.java
+++ src/com/igl/VectorHelper.java
@@ -55,4 +55,16 @@ public class VectorHelper {
               }
               }
 
+    public static int[] maxAndMin(int[] v)
+     {
+                int res[] = new int[2];
+                res[0] = res[1] = v[0];
+                for(int i = 1; i < v.length; i++)
+                    {
+                        if(v[i] > res[0]) res[0] = v[i];
+                        if(v[i] < res[1]) res[1] = v[i];
+                    }
+              return res;
+
+     }
 }
